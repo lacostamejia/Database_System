fdeb2b64f9961a88aaf572451b6d52a1d1b78731
Victor Petrillo <17276460+Zeekeybookeydo@users.noreply.github.com>
2022-11-25 18:49:24 -0500

    Restructure files

--- survey.js
+++
@@ -1,9 +0,0 @@
-
-function addItem(){
-    var li = document.createElement("LI");  
-    var input = document.getElementById("name_survey");
-    li.innerHTML = input.value;
-    input.value = "";
-
-    document.getElementById("surveys").appendChild(li);
-}
