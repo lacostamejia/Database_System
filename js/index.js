fd5f7c0a0a382bb3c08fa312e755eb9372a8ca45
Victor Petrillo <17276460+Zeekeybookeydo@users.noreply.github.com>
2022-11-26 02:12:08 -0500

    Formatting, login, and save cookie

--- js/index.js
+++ js/index.js
@@ -4,29 +4,29 @@ const container = document.querySelector(".container"),
   signUp = document.querySelector(".signup-link"),
   login = document.querySelector(".login-link");
 
-const urlBase = "http://petrillo.zone/api";
+const urlBase = "https://petrillo.zone/api";
 const ext = "php";
 
 // js code to show/hide password and change icon
-pwShowHide.forEach(eyeIcon => {
+pwShowHide.forEach((eyeIcon) => {
   eyeIcon.addEventListener("click", () => {
-    pwFields.forEach(pwField => {
+    pwFields.forEach((pwField) => {
       if (pwField.type === "password") {
         pwField.type = "text";
 
-        pwShowHide.forEach(icon => {
+        pwShowHide.forEach((icon) => {
           icon.classList.replace("uil-eye-slash", "uil-eye");
-        })
+        });
       } else {
         pwField.type = "password";
 
-        pwShowHide.forEach(icon => {
+        pwShowHide.forEach((icon) => {
           icon.classList.replace("uil-eye", "uil-eye-slash");
-        })
+        });
       }
-    })
-  })
-})
+    });
+  });
+});
 
 // js code to appear signup and login form
 // Login form -> register form
@@ -41,15 +41,14 @@ login.addEventListener("click", () => {
 
 // Login button
 function login_function() {
-
   // Result span for errors
-  var result = document.getElementById('login-result');
-  result.textContent = '';
+  var result = document.getElementById("login-result");
+  result.textContent = "";
 
   // Get form
-  var form = document.getElementById('login-form');
+  var form = document.getElementById("login-form");
   var data = new FormData(form);
-  
+
   // Handle form entry
   for (const [key, value] of data) {
     // console.log(`${key}: ${value}`);
@@ -58,14 +57,17 @@ function login_function() {
     data[key] = value.trim();
 
     // Check if entires are empty
-    if (value == '') {
+    if (value == "") {
       result.textContent = `${key} is empty`;
       return;
     }
 
     // Checks the email with regex
-    if (data[key] == data['Email'] && !data['Email'].match(/(.+)@((.+){2,})\.((.+){2,})/)) {
-      result.textContent = 'Please enter a valid email';
+    if (
+      data[key] == data["Email"] &&
+      !data["Email"].match(/(.+)@((.+){2,})\.((.+){2,})/)
+    ) {
+      result.textContent = "Please enter a valid email";
       return;
     }
   }
@@ -79,26 +81,54 @@ function login_function() {
 
   // Handle api call
   var xhr = new XMLHttpRequest();
-  url = urlBase + '/login.' + ext;
-  xhr.open('POST', url, true);
+  url = urlBase + "/login." + ext;
+  xhr.open("POST", url, true);
   xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
   try {
-
     // Wait for async return
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
       // Handle the return from api
-      console.log('something happened')
-      console.log(this.readyState);
-      console.log(this.status)
-    }
+      if (this.readyState == 4 && this.status == 200) {
+        let returnJson = JSON.parse(xhr.responseText);
+
+        // Check for error
+        if (returnJson.error !== "") {
+          result.textContent = returnJson.error;
+          return;
+        }
+
+        // Saves the UserID
+        UserID = returnJson.UserID;
+
+        if (UserID < 1) {
+          result.textContent = "Email/Password combination incorrect";
+          return;
+        }
+
+        // Saves the users first and last name
+        FirstName = returnJson.FirstName;
+        LastName = returnJson.LastName;
+
+        // Save cookie using ID, first name, and last name.
+        saveCookie();
+
+        // Move to the next html page surveys
+        // location.href = "survey.html";
+      }
+    };
 
     // Send form
     xhr.send(formJson);
-  }
-  catch (err) {
+  } catch (err) {
     // Display error
     result.textContent = err.message;
   }
-  // Move to the 
-  // location.href = 'survey.html';
+}
+
+// Save a cookie with UserID, FirstName, LastName
+function saveCookie() {
+  let minutes = 20;
+  let date = new Date();
+  date.setTime(date.getTime() + minutes * 60 * 1000);
+  document.cookie = "FirstName=" + FirstName + ",LastName=" + LastName + ",UserID=" + UserID + ";expires=" + date.toGMTString();
 }
