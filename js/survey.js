350ec46e215f2dcfc998af66bbc7dfe4463c5ba8
Luis Acosta <lacostamejia@knights.ucf.edu>
2022-11-26 16:46:35 -0500

    Added number of questions, end date, and type of questions frontend

--- js/survey.js
+++ js/survey.js
@@ -7,3 +7,34 @@ function addItem(){
 
     document.getElementById("surveys").appendChild(li);
 }
+
+const first = document.getElementById("first")
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+
+function first_choice(){
+    if(first.style.display === "none"){
+        first.style.display = "block";
+    }
+    else{
+        first.style.display = "none";
+    }
+};
+
+function second_choice(){
+    if(second.style.display === "none"){
+        second.style.display = "block";
+    }
+    else{
+        second.style.display = "none";
+    }
+};
+
+function third_choice(){
+    if(third.style.display === "none"){
+        third.style.display = "block";
+    }
+    else{
+        third.style.display = "none";
+    }
+};
