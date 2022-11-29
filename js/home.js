b34482d5f3333aea5a65d9c4f80b18fe6aa4e445
Victor Petrillo <17276460+Zeekeybookeydo@users.noreply.github.com>
2022-11-29 08:28:33 -0500

    get created surveys js and html

--- js/home.js
+++ js/home.js
@@ -4,6 +4,7 @@ const ext = "php";
 
 document.addEventListener("DOMContentLoaded", function () {
     welcome_function();
+    createdSurveys();
 });
 
 function welcome_function() {
@@ -11,7 +12,7 @@ function welcome_function() {
     username.textContent = "";
 
     // Makes sure UserID exists thus cookie exists
-    if (readCookieAttr("UserID") !== 0) {
+    if (document.cookie != "" && readCookieAttr("UserID") > 0) {
         username.textContent = `Welcome, ${readCookieAttr('FirstName')} ${readCookieAttr('LastName')}`;
     }
 }
@@ -28,4 +29,134 @@ function readCookieAttr(attribute) {
         .split(',')
         .find((row) => row.startsWith(`${attribute}=`))
         ?.split('=')[1];
-} 
+}
+
+// Return the cookie as an object
+function cookieToObject() {
+
+    const result = {}
+
+    var str = document.cookie;
+    str = str.split(',');
+    for (let i in str) {
+        const cur = str[i].split('=');
+        result[cur[0]] = cur[1];
+    }
+
+    return result;
+}
+
+// Update the cookie's expiration time
+function updateCookie() {
+    var cookie = document.cookie;
+}
+
+// Gets and creates a table of user created surveys
+function createdSurveys() {
+
+    // Get the div to create the list
+    var createdSurvey = document.getElementById("created-surveys");
+
+    // Result span for errors
+    var result = document.createElement('span');
+    result.classList.add("invalid");
+    result.textContent = "";
+    createdSurvey.appendChild(result);
+
+    // Convert to JSON
+    const sendJson = JSON.stringify(cookieToObject());
+    // console.log(sendJson);
+
+    // Handle api call
+    var xhr = new XMLHttpRequest();
+    url = urlBase + "/getCreatedSurvey." + ext;
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+        // Wait for async return
+        xhr.onreadystatechange = function () {
+            // Handle the return from api
+            if (this.readyState == 4 && this.status == 200) {
+                // Have to double parse the response to ensure response is turned into object
+                let returnJson = JSON.parse(JSON.parse(xhr.responseText));
+                // console.log(returnJson);
+
+                // Check for error
+                if (returnJson.error !== "" && returnJson.error !== "No Records Found") {
+                    result.textContent = returnJson.error;
+                    return;
+                }
+
+                // Turn the returned json to table
+                const table = document.createElement('table');
+                table.classList.add('survey-table');
+                createdSurvey.appendChild(table);
+
+                // Append the row
+                var row = document.createElement('tr');
+                table.appendChild(row);
+
+                // Append each column
+                let cell = document.createElement('td');
+                cell.textContent = "Title";
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = "Description";
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = "Start Date";
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = "End Date";
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = "# Type1";
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = "# Type2";
+                row.appendChild(cell);
+                cell = document.createElement('td');
+                cell.textContent = "Delete Survey";
+                row.appendChild(cell);
+
+                returnJson.Surveys.forEach(element => {
+                    // console.log(element);
+                    let row = document.createElement('tr');
+                    row.id = `SurveyID=${element.SurveyID}`;
+                    table.appendChild(row);
+
+                    for (const [key, value] of Object.entries(element)) {
+                        // console.log(`${key}: ${value}`);
+
+                        if (key === "SurveyID") {
+                            let btn = document.createElement('input');
+                            btn.type = "button";
+                            btn.className = "del-button";
+                            btn.value = "Delete";
+                            btn.onclick = function () { delete_Survey(value) };
+                            row.appendChild(btn);
+                            continue;
+                        }
+
+                        let cell = document.createElement('td');
+                        cell.textContent = value;
+                        row.appendChild(cell);
+                    }
+                });
+
+                // Update cookie expiration time
+                updateCookie();
+            }
+        };
+
+        // Send Json
+        xhr.send(sendJson);
+    } catch (err) {
+        // Display error
+        result.textContent = err.message;
+    }
+}
+
+function delete_Survey(val) {
+    console.log(val);
+}
