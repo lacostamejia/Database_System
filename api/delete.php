3d8d0993048b8c39b2e8e55c5442f60025d219c8
sanchezc47 <84475622+sanchezc47@users.noreply.github.com>
2022-11-29 19:21:19 -0500

    tweaks

--- api/delete.php
+++ api/delete.php
@@ -8,11 +8,7 @@ if (isset($_GET["SurveyID"])){
     $database = new Database();
     $db = $database->connect();
 
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-    header('Content-Type: application/json');
+
 
     $sql = "DELETE FROM results WHERE SurveyID= $SurveyID";
     $connection->query($sql);
