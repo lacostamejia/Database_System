017e0fbfde42f8efe8e2667c733ed92da1548308
Victor Petrillo <17276460+Zeekeybookeydo@users.noreply.github.com>
2022-12-05 22:11:59 -0500

    typo fix

--- api/getSurveyResults.php
+++ api/getSurveyResults.php
@@ -19,7 +19,7 @@ $inData = json_decode(file_get_contents('php://input'), true);
 // Try to perfrom the query
 try {
     // Get all of the results from the surveyid and relevant survey information
-    $stmt = $db->prepare("SELECT s.Title,s.Description,s.NumType1,s.NumType2,s.Type1Q,r.Type1A1,s.Type1Q2,r.Type1A2,s.Type1Q3,r.Type1A3,s.Type1Q4,r.Type1A4,s.Type1Q5,r.Type1A5,
+    $stmt = $db->prepare("SELECT s.Title,s.Description,s.NumType1,s.NumType2,s.Type1Q1,r.Type1A1,s.Type1Q2,r.Type1A2,s.Type1Q3,r.Type1A3,s.Type1Q4,r.Type1A4,s.Type1Q5,r.Type1A5,
                                     s.Type2Q1,r.Type2A1,s.Type2Q2,r.Type2A2,s.Type2Q3,r.Type2A3,s.Type2Q4,r.Type2A4,s.Type2Q5,r.Type2A5 
                         FROM results r, surveys s 
                         WHERE r.surveyid=? and s.surveyid=?;");
