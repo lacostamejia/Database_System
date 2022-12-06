87b75d8879b7b414f1aa35a58c8d79e9c9def534
Christopher Sanchez <84475622+sanchezc47@users.noreply.github.com>
2022-12-05 20:51:46 -0500

    Add files via upload (#3)
    * Add files via upload
    
    minor visual changes
    added report and survey list pages and linked to home html
    need api/logic for report and survey
    
    * Add files via upload
    
    minor visual changes
    added report and survey list pages and linked to home html
    need api/logic for report and survey
    
    Co-authored-by: Victor Petrillo <17276460+Zeekeybookeydo@users.noreply.github.com>

---
+++ surveyresults.php
@@ -0,0 +1,53 @@
+<?php
+
+// Include database class
+include_once './Database.php';
+
+// Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+// Headers
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+header('Content-Type: application/json');
+
+// Convert incoming json to variable
+$inData = json_decode(file_get_contents('php://input'), true);
+
+// Try to perfrom the query
+try {
+    // Get all user emails
+    $stmt = $db->query("SELECT Title FROM surveys WHERE CreatorID=?");
+
+    // Execute query
+    $stmt->execute();
+
+    // Get the results
+    $result = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+
+    // As long as result isnt empty
+    if (!empty($result)) {
+
+        // Return the list of all emails
+        $retValue = '{"Survey List": [';
+
+        foreach ($result as $value[0]) {
+            $retValue .= "\"$value[0]}\",";
+        }
+
+        // Remove the last comma from printing the array
+        $retValue = substr($retValue, 0, -1);
+
+        $retValue .= '],"error":""}';
+
+        echo $retValue;
+    } else {
+        $retValue = '{"Survey": "","error":"No Surveys available"}';
+        echo $retValue;
+    }
+} catch (PDOException $e) {
+    $retValue = '{"error":"' . $e->getMessage() . '"}';
+    echo $retValue;
+}
